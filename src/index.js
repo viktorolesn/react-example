3d6296d802a99fd196c8336aff62bc3a37d7db1a
Viktor Olesnevych <viktor.olesnevych@hotmail.com>
2021-01-19 22:41:25 -0600

    store added as one object to state

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
 import * as serviceWorker from "./serviceWorker";
-import state, { subscribe } from "./redux/state";
+import store from "./redux/state";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { addPost } from "./redux/state";
-import { updateNewPostText } from "./redux/state";
 import { BrowserRouter } from "react-router-dom";
 
 let rerenderAll = (state) => {
   ReactDOM.render(
     <BrowserRouter>
       <App
-        state={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
+        state={store.getState()}
+        addPost={store.addPost}
+        updateNewPostText={store.updateNewPostText}
       />
     </BrowserRouter>,
     document.getElementById("root")
   );
 };
 
-rerenderAll(state);
+rerenderAll(store.getState());
 
-subscribe(rerenderAll);
+store.subscribe(rerenderAll);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,46 +1,51 @@
-let rerenderAll = () => {};
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi, how are you", likesCount: "0" },
-      { id: 2, message: "It's my first post", likesCount: "23" },
-      { id: 3, message: "Wowowo", likesCount: "89" },
-    ],
-    postElement: "",
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how are you", likesCount: "0" },
+        { id: 2, message: "It's my first post", likesCount: "23" },
+        { id: 3, message: "Wowowo", likesCount: "89" },
+      ],
+      postElement: "",
+    },
+    messagesPage: {
+      dialogs: [
+        { id: "1", username: "Viktor" },
+        { id: "3", username: "Alex" },
+        { id: "2", username: "John" },
+        { id: "4", username: "Frank" },
+        { id: "5", username: "Maxym" },
+      ],
+      messages: [
+        { id: "1", message: "Hi" },
+        { id: "2", message: "How are you today" },
+        { id: "3", message: "All good, how about you? What's new around?" },
+      ],
+    },
   },
-  messagesPage: {
-    dialogs: [
-      { id: "1", username: "Viktor" },
-      { id: "3", username: "Alex" },
-      { id: "2", username: "John" },
-      { id: "4", username: "Frank" },
-      { id: "5", username: "Maxym" },
-    ],
-    messages: [
-      { id: "1", message: "Hi" },
-      { id: "2", message: "How are you today" },
-      { id: "3", message: "All good, how about you? What's new around?" },
-    ],
+  getState() {
+    return this._state;
+  },
+  _callSubscriber() {},
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.postElement,
+      likesCount: 0,
+    };
+
+    this._state.profilePage.posts.push(newPost);
+    this.updateNewPostText("");
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.postElement = newText;
+    this._callSubscriber(this._state);
+  },
+
+  subscribe(observer) {
+    this._callSubscriber = observer;
   },
 };
 
-export let addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.postElement,
-    likesCount: 0,
-  };
-
-  state.profilePage.posts.push(newPost);
-  updateNewPostText("");
-};
-
-export let updateNewPostText = (newText) => {
-  state.profilePage.postElement = newText;
-  rerenderAll(state);
-};
-
-export let subscribe = (observer) => {
-  rerenderAll = observer;
-};
-export default state;
+export default store;
+window.store = store;
